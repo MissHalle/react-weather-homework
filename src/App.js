cdf4df5f9b37619042eb6822780cf5eb49cb5456
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-06-27 10:35:53 -0500

    adding bootstrap by npm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        React Weather Application created by Halle Gregory and
-        <a
-          href="https://github.com/MissHalle/react-weather-homework"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          React Weather Application created by Halle Gregory and
+          <a
+            href="https://github.com/MissHalle/react-weather-homework"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
